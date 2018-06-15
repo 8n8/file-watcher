8b98bb6caf0f56634f138c62f759be7f2934605c
True Ghiassi <true@ghiassitrio.co.uk>
2018-06-15 17:28:25 +0100

    First pass of io function in watcher is done.

--- go/src/gochallenge/watcher/watcher.go
+++ go/src/gochallenge/watcher/watcher.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/fsnotify/fsnotify"
+import (
+	"github.com/fsnotify/fsnotify"
+	"net/http"
+	"bytes"
+	"io/ioutil"
+	"log"
+)
 
 type fileSet map[string]string
 
@@ -16,7 +22,8 @@ func initialState() stateT {
 }
 
 type outputT struct {
-	x int
+	jsonToSend []byte
+	msgToPrint string
 }
 
 type inputT struct {
@@ -28,27 +35,59 @@ func stateToOutput(state stateT) outputT {
 }
 
 type ioResultT struct {
-	x int
+	fileEvents []fsnotify.Event
+	fileErrors []error
+	errChOk bool
+	eventChOk bool
+	requestErr error
+	responseBody []byte
+	readBodyErr error
 }
 
-func io(eventCh chan fsnotify.Event, errCh chan error, output outputT) ioResultT {
-	return ioResultT{x: 0}
+func initIoResult() ioResultT {
+	return ioResultT{
+		fileEvents: []fsnotify.Event,
+		fileErrors: []error,
+		errChOk: nil,
+		eventChOk: nil,
+		requestErr: nil,
+		responseBody: nil,
+		readBodyErr: nil,
+	}
 }
 
-func fileWatcher(eventCh chan fsnotify.Event, errCh chan error, folderName string) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		errCh <- err
-		return
+func io(eventCh chan fsnotify.Event, errCh chan error, askCh chan bool, output outputT, masterUrl string) ioResultT {
+
+	if output.msgToPrint != nil {
+		log.Print(output.msgToPrint)
 	}
-	for {
-		select {
-		case event := <-watcher.Events:
-			eventCh <- event
-		case err := <-watcher.Errors:
-			errCh <- err
-		}
-	}
+
+	result := initIoResult()
+
+	askCh <- true
+
+	events, eventsChOk := <-eventCh
+	result.eventChOk = eventsChOk
+	if !eventsChOk { return result }
+	result.fileEvents = append(result.fileEvents, events)
+
+	errors, errChOk := <- errCh
+	result.errChOk = errChOk
+	if !errChOk { return result }
+	result.fileErrors = append(result.fileErrors, errors)
+
+	response, postErr := http.Post(
+		masterUrl,
+		"application/json",
+		bytes.NewBuffer(output.jsonToSend))
+	defer response.Body.Close()
+	result.requestErr = postErr
+	if postErr != nil { return result }
+	body, bodyErr := ioutil.ReadAll(response.Body)
+	result.responseBody = body
+	result.readBodyErr = bodyErr
+
+	return result
 }
 
 func update(state stateT, ioResult ioResultT) stateT {
@@ -57,9 +96,29 @@ func update(state stateT, ioResult ioResultT) stateT {
 
 func main() {
 
-	eventCh := make(chan fsnotify.Event, 10000)
-	errCh := make(chan error, 10000)
-	go fileWatcher(eventCh, errCh, "/home/true/toWatch")
+	eventCh := make(chan []fsnotify.Event)
+	errCh := make(chan []error)
+	askCh := make(chan bool)
+	go func() {
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			errCh <- err
+			return
+		}
+		var events []fsnotify.Event
+		var errors []error
+		for {
+			select {
+			case event := <-watcher.Events:
+				events = append(events, event)
+			case err := <-watcher.Errors:
+				errors = append(errors, err)
+			case <-askCh:
+				eventCh <- events
+				errCh <- errors
+			}
+		}
+	}()
 
 	state := initialState()
 	for state.keepGoing {
